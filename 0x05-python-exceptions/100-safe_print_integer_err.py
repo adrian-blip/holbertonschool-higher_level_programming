c831f64c8408301068616f5ebf6c474e5375b5c5
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-05-17 23:10:17 -0500

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import sys
 
-def safe_print_integer_err(value):
 
-  def safe_print_integer(value):
+def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return(True)
     except Exception as idon:
         print("Exception: {}".format(idon), file=sys.stderr)
         return False
+
+     
+
