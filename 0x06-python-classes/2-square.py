3003ea5f463d8d3d6348ed8b0b76d777804b705d
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-05-18 16:22:41 -0500

    afg

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
             size (__int__): Size of the class square
         """
 
-        if type(size) is not  int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
