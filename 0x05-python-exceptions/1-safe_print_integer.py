1595a7bcee8eb3424c5bf623603b142164987e99
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-05-17 22:48:32 -0500

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
 
+
+def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        
         return(True)
     except:
-        
-        
         return(False)
-    
+
