87a9a57701a48bcd2c7e7c1c89b8e18359c12ffe
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-05-17 22:51:27 -0500

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_division(a, b):
     try:
         div = a / b
@@ -9,6 +10,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(div))
     return(div)
-        
-        
-        
