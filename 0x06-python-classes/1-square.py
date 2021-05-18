427b7f38b9612f14176b0449dd6ec51bc9dd8ae4
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-05-18 15:37:54 -0500

    df

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""Define class Square"""
+
+class Square:
+
+ """ Defines a square
+    Args:
+        size (__int__): Size of the class square
+    """
+   def __init __ (self, size): 
+      self. __ size = size    
