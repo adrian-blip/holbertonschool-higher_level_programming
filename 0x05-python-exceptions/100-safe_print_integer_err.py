ea575084138661ee4f039938d8ce37b9bdbe1e80
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-05-17 23:08:36 -0500

    Create  100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+
+  def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return(True)
+    except Exception as idon:
+        print("Exception: {}".format(idon), file=sys.stderr)
+        return False
