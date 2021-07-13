c12e8185b7c6a9bb8c0eb546e58a383e0decc081
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-07-13 16:36:53 -0500

    Create a new user and grant all permissions

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- We create a new user and grant all permissions
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
