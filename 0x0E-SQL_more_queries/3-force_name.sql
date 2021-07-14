aed8189f9d8c105c66427a6b25b363239f1c5bbf
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-07-13 21:20:28 -0500

    space

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,4 @@
 -- Create the table force_name
-CREATE TABLE IF NOT EXISTS force_name(Id INT, name NOT NULL VARCHAR(256));
+
+CREATE TABLE IF NOT EXISTS force_name
+(Id INT, name NOT NULL VARCHAR(256));
