677f16c23e2bcee2fcb6e75d35bb9fac5344facd
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-07-13 20:46:15 -0500

    Update 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- Create the database hbtn_0d_usa and the table states
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
-CREATE TABLE IF NOT EXITS hbtn_0d_usa.states(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) UNIQUE);
+CREATE TABLE IF NOT EXITS hbtn_0d_usa.states(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
