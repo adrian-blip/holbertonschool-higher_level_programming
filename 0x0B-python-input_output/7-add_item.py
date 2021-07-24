907b60d190513260ec78df6b04abe1aac200b5b1
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-07-23 21:52:14 -0500

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""Program to add load and save a add_item.json file"""
+# 7-save_to_json_file.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a JSON file-writing function."""
+import json
 
-import sys
 
-
-if __name__ == "__main__":
-
-    save = __import__('5-save_to_json_file').save_to_json_file
-    load = __import__('6-load_from_json_file').load_from_json_file
-
-    try:
-        l_items = load("add_item.json")
-    except FileNotFoundError:
-        l_items = []
-
-    l_items.extend(sys.argv[1:])
-    save(l_items, "add_item.json")
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
