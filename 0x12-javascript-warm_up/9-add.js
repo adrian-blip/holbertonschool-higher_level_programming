e51eaabcc75a99d35a4ffdfabbe6c28987297e47
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-08-02 21:41:46 -0500

    Create 9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* Script that prints the addition of 2 integers */
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+function add (a, b) {
+  return a + b;
+}
+console.log(add(num1, num2));
