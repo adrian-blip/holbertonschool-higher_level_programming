fb4331bbb790353d3490e1cc947c3f56f003fc79
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-08-03 16:50:46 -0500

    Create 1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* Empty class Rectangle that defines a rectangle */
+class Rectangle(h,w) {
+  constructor(h, w) {
+    this.h = h;
+    this.w = w;
+  }
+}
+module.exports = Rectangle;
