288be70339684faf410658f837f3d1bc5aa950f1
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-08-07 14:58:03 -0500

    Firtspoint

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""ConsultMysqldb"""
+
+if __name__ == '__main__':
+    """main"""
+
+    import MySQLdb
+    import sys
+
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT name FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
