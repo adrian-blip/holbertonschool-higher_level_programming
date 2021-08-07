3454d02438c5e5adb29acc1c7b7a991ce904f555
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-08-07 16:03:53 -0500

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument."""
+"""take in an argument and displays all values in the states table"""
 
 if __name__ == '__main__':
     """main"""
