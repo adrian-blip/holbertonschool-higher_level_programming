5f4adf8f801178e57b268cda8231f7dae36ae1fe
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-08-07 16:16:15 -0500

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,4 +1,4 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """takes in arguments and displays all values
 in the states table of hbtn_0e_0_usa where name 
 matches the argument. But this time, write one that
