624e9b35426ad1811d815babfdf77d6512b098c6
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-08-07 15:01:57 -0500

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,8 @@ if __name__ == '__main__':
                            user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT name FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
+    cur.execute
+    ("SELECT name FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
