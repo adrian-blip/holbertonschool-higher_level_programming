ae2ba148c88983e4e971cf029cba133f31ac38a8
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-08-07 15:56:13 -0500

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,19 @@
+#!/usr/bin/python3
 
+"""ConsultMysqldb"""
+
+if __name__ == '__main__':
+    """main"""
+
+    import MySQLdb
+    import sys
+
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                ORDER BY states.id ASC".format(argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
